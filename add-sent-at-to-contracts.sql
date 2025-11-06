10a4b602f668eb2a63e7a4a026f847034d222e65
Juanma Garrido <juangpdev@gmail.com>
2025-11-06 10:09:26 +0100

    update and delete files

--- add-sent-at-to-contracts.sql
+++
@@ -1,3 +0,0 @@
--- Agregar columna sent_at a la tabla contracts
-ALTER TABLE contracts 
-ADD COLUMN sent_at TIMESTAMP WITH TIME ZONE;
--- check-time-entries-for-user.sql
+++
@@ -1,9 +0,0 @@
--- Consulta para ver si un usuario tiene registros de time_entries
--- Reemplaza 'USER_ID_AQUI' por el id real del usuario nuevo
-
-SELECT *
-FROM time_entries
-WHERE user_id = 'USER_ID_AQUI'
-ORDER BY start_time DESC;
-
--- Si no devuelve filas, el usuario realmente no tiene registros de tiempo.
